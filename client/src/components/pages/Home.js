04d6cee871314367d280783df34b7519158184ba
Tommy Adebiyi <13374036+tomadto@users.noreply.github.com>
2021-01-17 16:01:30 -0500

    single page routing made a bit more intuitive

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -7,64 +7,75 @@ import "./Home.css";
 
 import About from "../modules/About.jsx";
 import Projects from "../modules/Projects.jsx";
+import NotFound from "../pages/NotFound.js";
 import lorem from "../modules/lorem.json";
 
 const Home = (props) => {
+  const pageToNumber = (page) => {
+    if (page === "about") {
+      return 0;
+    } else if (page === "projects") {
+      return 1;
+    } else if (page === "fun") {
+      return 2;
+    } else {
+      return "NotFound";
+    }
+  };
+  const pages = [<About />, <Projects />];
   const [content, selectContent] = useState(() => {
     if (props.place) {
-      return props.place;
+      return pageToNumber(props.place);
     } else {
       return 0;
     }
   });
-  const pages = [<About />, <Projects />];
+
   const handleClick = (e) => {
-    if (e.target.textContent === "about") {
-      selectContent(0);
-    } else if (e.target.textContent === "projects") {
-      selectContent(1);
-    } else if (e.target.textContent === "fun") {
-      selectContent(2);
-    }
+    selectContent(pageToNumber(e.target.textContent));
   };
-  return (
-    <>
-      <div className="u-flex Home-miniNav-Container">
-        <div className="Home-emptySpace" />
-        <div className="Home-miniNav u-flex u-flex-justifyCenter">
-          <Link
-            to="/0"
-            // TODO: make this and similar a word, if only I was using typescript and could use enums...
-            className={`${content === 0 ? "u-textDark " : "u-textGrey "}u-textCenter Home-link`}
-            onClick={handleClick}
-          >
-            about
-          </Link>
-          <Link
-            to="/1"
-            className={`${content === 1 ? "u-textDark " : "u-textGrey "}u-textCenter Home-link`}
-            onClick={handleClick}
-          >
-            projects
-          </Link>
-          <Link
-            to="/2"
-            className={`${content === 2 ? "u-textDark " : "u-textGrey "}u-textCenter Home-link`}
-            onClick={handleClick}
-          >
-            fun
-          </Link>
+  if (content === "NotFound") {
+    return <NotFound />;
+  } else {
+    return (
+      <>
+        <div className="u-flex Home-miniNav-Container">
+          <div className="Home-emptySpace" />
+          <div className="Home-miniNav u-flex u-flex-justifyCenter">
+            <Link
+              to="/about"
+              // TODO: make this and similar a word, if only I was using typescript and could use enums...
+              className={`${content === 0 ? "u-textDark " : "u-textGrey "}u-textCenter Home-link`}
+              onClick={handleClick}
+            >
+              about
+            </Link>
+            <Link
+              to="/projects"
+              className={`${content === 1 ? "u-textDark " : "u-textGrey "}u-textCenter Home-link`}
+              onClick={handleClick}
+            >
+              projects
+            </Link>
+            <Link
+              to="/fun"
+              className={`${content === 2 ? "u-textDark " : "u-textGrey "}u-textCenter Home-link`}
+              onClick={handleClick}
+            >
+              fun
+            </Link>
+          </div>
+          <div className="Home-emptySpace" />
         </div>
-        <div className="Home-emptySpace" />
-      </div>
-      <hr className="Home-horizontalLine" />
-      <div className="Home-container u-flex">
-        {/* <div className="Home-emptySpace"/> */}
-        {pages[content]}
-        {/* <div className="Home-emptySpace"/> */}
-      </div>
-    </>
-  );
+        <hr className="Home-horizontalLine" />
+        <div className="Home-container u-flex">
+          {/* <div className="Home-emptySpace"/> */}
+          {pages[content]}
+          {/* <div className="Home-emptySpace"/> */}
+        </div>
+      </>
+    );
+  }
 };
 
 export default Home;
