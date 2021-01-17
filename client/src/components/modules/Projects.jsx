b24b719f0ebcdd63bc5a02470f6ea1669606bb76
Tommy Adebiyi <13374036+tomadto@users.noreply.github.com>
2021-01-17 01:45:00 -0500

    test card css

--- client/src/components/modules/Projects.jsx
+++ client/src/components/modules/Projects.jsx
@@ -1,10 +1,30 @@
 import React, { useState, useEffect, useCallback } from "react";
-import "../pages/Home.css";
+// import "../pages/Home.css";
 import "../../utilities.css";
+import "./Projects.css";
 import lorem from "./lorem.json";
 
 const Projects = (props) => {
-  return <div className="Home-text">lol</div>;
+  return (
+    <div className="Projects-container">
+      <div className="Projects-emptySpace" />
+      <div className="Projects-cardContainer">
+        <div className="Projects-card">
+          <div className="Projects-cardInner"></div>
+        </div>
+        <div className="Projects-card">
+          <div className="Projects-cardInner"></div>
+        </div>
+        <div className="Projects-card"></div>
+        <div className="Projects-card"></div>
+        <div className="Projects-card"></div>
+        <div className="Projects-card"></div>
+        <div className="Projects-card"></div>
+      </div>
+      <div className="Projects-emptySpace" />
+    </div>
+  );
+  // return <div className="Home-text">lol</div>;
 };
 
 export default Projects;
